34b4c04ce6057c3d4a59ab6af418b296fffed7c5
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-19 13:40:03 +0300

    file added

---
+++ cars.js
@@ -0,0 +1,21 @@
+const Cars = function(make, model, colour, image, registrationNumber, price){
+this.make = make;
+this.model = model;
+this.colour = colour;
+this.image = image;
+this.registrationNumber = registrationNumber;
+this.price = price;
+// this.editPrice = function() {
+    
+// }
+}
+
+
+let car1 = new Cars("hyunday","kona","black","imge",384467,"2000$");
+let car2 = new Cars("hyunday","kona","red","imge",384467,"4500$")
+let car3 = new Cars("hyunday","kona","blue","imge",384467,"6800$")
+let car4 = new Cars("hyunday","kona","gray","imge",384467,"20900$")
+let car5 = new Cars("hyunday","kona","pink","imge",384467,"13000$")
+
+console.log(car1);
+
