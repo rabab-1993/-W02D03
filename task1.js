56aae5c5d7ede9bf760fbfd0eaa745b11587fe32
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-19 22:11:55 +0300

    updated

--- task1.js
+++ task1.js
@@ -10,7 +10,7 @@ let bttn = document.createElement("button")
 bttn.textContent = "change";
 cont.appendChild(bttn);
 
-
+// button event
 bttn.addEventListener("click",() =>{
    headings.style.fontSize = "6rem";
    par.style.fontStyle = "italic";
@@ -19,24 +19,42 @@ bttn.addEventListener("click",() =>{
   
 })
 // creat buttons 2
-let bttn2 = document.createElement("button")
-bttn2.textContent = "change image";
-cont.appendChild(bttn2);
+// let bttn2 = document.createElement("button")
+// bttn2.textContent = "change image";
+// cont.appendChild(bttn2);
 // bttn2.addEventListener("click",() =>{
 //    let imge2 = document.createElement("img");
 // //    imge2.setAttribute("src").inner
 // }
 
-/* <img src="./undraw_Time_management_re_tk5w.svg" alt="image" id="img"> */
 
+// input alert
 function alert1 () {
     alert("Hello")
 }
 
-// function loadPage() {
-//     alert("Don't load the page")
-// }
+// loading page alret
+function loadPage() {
+    alert("Don't load the page")
+}
 
 // paragraphs.addEventListener("mouseover",() => {
 //     style.background = "green";
 // })
+
+
+// image click right event
+
+img.addEventListener("contextmenu",() => {
+    alert("You clicked right")
+})
+
+
+// Change an image when the mouse moves onto it, and then back to the original image when the mouse moves off of it.
+img.addEventListener("mouseenter", () => {
+    img.style.background = "pink";
+})
+
+img.addEventListener("mouseleave", () => {
+    img.style.background = "none";
+})
